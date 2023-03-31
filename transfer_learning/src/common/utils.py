2bb788b9d609c150b26a23386484be960fccf499
Isak Falk <isak.falk@live.se>
2023-03-31 11:45:31 +0200

    Add new subdir for our project

---
+++ transfer_learning/src/common/utils.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from ocpmodels.preprocessing import AtomsToGraphs
+from torch_geometric.data import Batch
+import ase.io
+from typing import Tuple
+
+# Keyword arguments for converting ASE Atoms objects to PyTorch Geometric Batch objects per model
+ATOMS_TO_GRAPH_KWARGS = {
+    "schnet": dict(
+        max_neigh=50,
+        radius=6,
+        r_energy=True,
+        r_forces=True,
+        r_distances=False,
+        r_edges=True,
+        r_fixed=True,
+    )
+}
+
+def load_xyz_to_pyg_batch(path: Path, atoms_to_graph_kwargs: dict) -> Tuple[Atoms, Batch, int, int]:
+    """
+    Load XYZ data from a given path using ASE and convert it into a PyTorch Geometric Batch object.
+
+    Args:
+        path (Path): Path to the XYZ data file.
+        **atoms_to_graph_kwargs: Optional keyword arguments for AtomsToGraphs class.
+
+    Returns:
+        Tuple consisting of raw_data, data_batch, num_frames, and num_atoms.
+        raw_data (Atoms): Raw data loaded from the file using ASE.
+        data_batch (Batch): Batch object containing converted data for all frames.
+        num_frames (int): Number of frames in the loaded XYZ data file.
+        num_atoms (int): Number of atoms in each frame.
+    """
+    raw_data = ase.io.read(path, index=":")
+    num_frames = len(raw_data)
+    a2g = AtomsToGraphs(
+        **atoms_to_graph_kwargs,
+    )
+    data_object = a2g.convert_all(raw_data, disable_tqdm=True)
+    data_batch = Batch.from_data_list(data_object)
+    num_atoms = data_batch[0].num_nodes
+    return raw_data, data_batch, num_frames, num_atoms
